2453b98ae7c04e0ef86f765b9234b796c6b6197d
Miguel Angel <vite_celeste@hotmail.com>
2018-03-18 23:50:56 -0600

    Añadidas clases

---
+++ src/java/BaseDatos/CrudAnimales.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaseDatos;
+
+/**
+ *
+ * @author Miguel Angel
+ */
+public class CrudAnimales extends Conexion{
+    public void insertarAnimales (Animales animal){//INSERTAR ANIMALES
+        try{
+            conexionbd();
+            String insertar ="call zoologico.insertar_animales('"+animal.getId()+"', '"+animal.getNombre()+"', '"+animal.getEspecie()+"', '"+animal.getPaisOrigen()+"', '"+animal.getEstatus()+"', '"+animal.getPeso()+"', '"+animal.getAltura()+"', '"+animal.getDieta()+"', '"+animal.getSexo()+"', '"+animal.getNivelRiesgo()+"', '"+animal.getNivelRiesgo()+"', '"+animal.getIDHabitad()+"', '"+animal.getPresProp()+"');";
+            sentencia.execute(insertar);
+        }catch(Exception e){
+            
+        }
+    }
+    
+   public void consultarAnilames (Animales animal){//CONSULTAR ANIMALES
+       try{
+            conexionbd();
+            String cosultar ="select *from animales;";
+            sentencia.execute(cosultar);
+        }catch(Exception e){
+            
+        }
+   }
+}
