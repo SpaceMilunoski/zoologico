591a3893eee5ee0e9fd082906c1699ff83a91b08
SpaseMilunoski <dan.soto576@gmail.com>
2018-03-20 23:50:46 -0600

    crudPrestados,permanentes

--- src/java/BaseDatos/Webservicezoo.java
+++ src/java/BaseDatos/Webservicezoo.java
@@ -71,15 +71,29 @@ public class Webservicezoo {
     @WebMethod(operationName = "agregarHabitad")
     public String agregarHabitad(@WebParam(name = "jsonHabitad") String jsonHabitad) {
         //TODO write your implementation code here:
+        JsonObject habitadj = Json.createReader(new StringReader(jsonHabitad)).readObject();
+        Habitad habitad = new Habitad(habitadj);
+        crudHabitad cHabitad = new crudHabitad();
+        try {
+            cHabitad.insertarHabitad(habitad);
+        } catch (Exception e) {
+        }
         return null;
     }
 
     /**
      * Web service operation
      */
+    crudPrestados_Permanentes cpp = new crudPrestados_Permanentes();
     @WebMethod(operationName = "Prestados")
     public String Prestados(@WebParam(name = "jPrestados") String jPrestados) {
         //TODO write your implementation code here:
+        JsonObject jprestados = Json.createReader(new StringReader(jPrestados)).readObject();
+        animalesPrestados prestados = new animalesPrestados(jprestados);
+        try {
+            cpp.insertarPrestados(prestados);
+        } catch (Exception e) {
+        }
         return null;
     }
 
@@ -88,6 +102,56 @@ public class Webservicezoo {
      */
     @WebMethod(operationName = "Permanentes")
     public String Permanentes(@WebParam(name = "jPermanentes") String jPermanentes) {
+        //TODO write your implementation code here:
+          JsonObject jpermanentes = Json.createReader(new StringReader(jPermanentes)).readObject();
+        animalesPermanentes permanentes = new animalesPermanentes(jpermanentes);
+        try {
+            cpp.insertarPermanentes(permanentes);
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "eliminarPermanetes")
+    public String eliminarPermanetes(@WebParam(name = "Id") String Id) {
+        //TODO write your implementation code here:
+        try {
+            cpp.eliminarPermanentes(Id);
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "eliminarPrestados")
+    public String eliminarPrestados(@WebParam(name = "Id") String Id) {
+        //TODO write your implementation code here:
+        try {
+            cpp.eliminarPrestados(Id);
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "agregarCuida")
+    public String agregarCuida(@WebParam(name = "jCuida") String jCuida) {
+        //TODO write your implementation code here:
+        return null;
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "eliminarCuida")
+    public String eliminarCuida(@WebParam(name = "Id") String Id) {
         //TODO write your implementation code here:
         return null;
     }
