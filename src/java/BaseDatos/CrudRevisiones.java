7ac1d20788c32cd4eb23733e3e56cf048fcb2e43
Miguel Angel <vite_celeste@hotmail.com>
2018-03-28 00:57:14 -0600

    Añadido crud de revisiones

--- src/java/BaseDatos/CrudRevisiones.java
+++ src/java/BaseDatos/CrudRevisiones.java
@@ -9,6 +9,44 @@ package BaseDatos;
  *
  * @author daniel
  */
-public class CrudRevisiones {
+public class CrudRevisiones extends Conexion{
+    public void insertarRevision(Revisiones revision){
+        try{
+            conexionbd();
+            String insertar ="call zoologico.insertarRevision('"+revision.getId()+"', '"+revision.getDescripcion()+"', '"+revision.getFechaIngreso()+"', '"+revision.getFechaSalida()+"', '"+revision.getTratamiento()+"', '"+revision.getObservaciones()+"', '"+revision.getEstatus()+"', '"+revision.getIdAnimal()+"', '"+revision.getConcentrado()+"', '"+revision.getIdVeterinario()+"');";
+            sentencia.execute(insertar);
+        }catch(Exception e){
+            
+        }
+    }
     
+    public void mostrarRevisiones(Revisiones revision){
+        try{
+            conexionbd();
+            String consultar ="select * from revisiones;";
+            sentencia.execute(consultar);
+        }catch(Exception e){
+            
+        }
+    }
+    
+    public void actualizarRevisiones(Revisiones revision){
+        try{
+            conexionbd();
+            String actualizar ="call zoologico.actualizarRevision('"+revision.getId()+"', '"+revision.getDescripcion()+"', '"+revision.getFechaIngreso()+"', '"+revision.getFechaSalida()+"', '"+revision.getTratamiento()+"', '"+revision.getObservaciones()+"', '"+revision.getEstatus()+"', '"+revision.getIdAnimal()+"', '"+revision.getConcentrado()+"', '"+revision.getIdVeterinario()+"');";
+            sentencia.execute(actualizar);
+        }catch(Exception e){
+            
+        }
+    }
+    
+    public void eliminarRevisiones(Revisiones revision){
+        try{
+            conexionbd();
+            String eliminar ="call eliminarRevision('"+revision.getId()+"');";
+            sentencia.execute(eliminar);
+        }catch(Exception e){
+            
+        }
+    }
 }
